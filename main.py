9b4f7eec2bb2bea91f7c5e63eb0f282cc526fa7f
Artem <artem.gavshin2459@gmail.com>
2024-04-11 10:48:30 +0300

    Исправил get_custom

--- main.py
+++ main.py
@@ -18,14 +18,6 @@ import queue
 # user_inp = f"{input()}"
 start_time = time.time()
 
-
-# Пока будет так, возможно будет функция, которая выдает какой-то общий результат
-# Наприме, информация о всех заголовках, ссылках и т.д.
-# Либо под каждую отельную задачу будет своя функция
-
-
-# Удобнее будет сделать под каждый хаголовок в таблице свою функци для сбора данных со страницы
-
 async def get_classes(url_of_site: str) -> list:
     """Асинхронная функция, собирающая названия всех классов на сайте"""
 
@@ -93,27 +85,6 @@ async def get_headers(url_of_site: str) -> str:
                 raise TypeError("Ошибка: Соединение не установлено, возможно неправильно указан URL сайта.")
     return f"Headings: {heading_list}\n h1: {soup.h1.text}, \t h2: {soup.h2.text}, \t h3: {soup.h3.text}, \t h4: {soup.h4.text}, \t h5: {soup.h5} , \t h6: {soup.h6.text}"
 
-
-# async def get_divs(url_of_site: str) -> list:
-#     """Функиця, собирающая все блочные элементы"""
-#     list_smth = []
-#     async with aiohttp.ClientSession() as session:
-#         async with session.get(url_of_site, headers={'UserAgent': UserAgent().chrome}) as response:
-#             print("Устанавливаю соединение...")
-#             await asyncio.sleep(0.5)
-#             if response.status == 200:
-#                 print("Соединение установлено")
-#                 html_text = await response.text(encoding='utf-8')
-#                 html_code = BeautifulSoup(html_text, "html.parser")
-#                 divs =  html_code.findAll("div")
-#                 list_smth.extend(divs)
-#
-#             else:
-#                 raise Exception("Ошибка: Соединение не установлено, возможно неправильно указан URL cайта.")
-#
-#     return list_smth
-
-
 async def get_title(url_of_site: str) -> list:
     """Асинхронная функция, собирающая все названия"""
     list_smth = []
@@ -141,9 +112,7 @@ predict_classes1 = (
     'logo'
 
 )
-async def get_custom_classes(url_of_site: str, predict_classes: tuple) -> list[ResultSet[Any]]:
-    """Асинхронная функция, собирающая названия всех классов на сайте и ищущая определённые классы"""
-
+async def get_custom_classes(url_of_site: str, predict_classes: tuple) -> list:
     async with aiohttp.ClientSession() as session:
         async with session.get(url_of_site, headers={'UserAgent': UserAgent().chrome}) as response:
             print("Устанавливаю соединение...")
@@ -159,16 +128,17 @@ async def get_custom_classes(url_of_site: str, predict_classes: tuple) -> list[R
                     classes = item['class']
                     list_smth.extend(classes)
 
+                result = []
+
                 for predict_class in predict_classes:
                     for i in range(len(list_smth)):
-                        if list_smth[i].find(predict_class) != -1:
-                            elements = html_code.findAll(class_=f'{list_smth[i]}')
-                            list_smth.append(elements)
+                        if list_smth[i] in predict_class:
+                            elements = html_code.find('div', {"class": f'{list_smth[i]}'})
+                            result.append(elements)
 
+                return result
             else:
                 raise TypeError("Ошибка: Соединение не установлено, возможно неправильно указан URL сайта.")
-
-    return list_smth
 
 
 #asyncio.run(get_title("https://qna.habr.com"))
@@ -204,59 +174,13 @@ async def get_custom_classes(url_of_site: str, predict_classes: tuple) -> list[R
 #    asyncio.set_event_loop_policy(asyncio.WindowsSelectorEventLoopPolicy())
 #    asyncio.run(main())
 
+# print(asyncio.run(get_classes("https://qna.habr.com")))
+# print(asyncio.run(get_title("https://qna.habr.com")))
+# print(asyncio.run(get_hrefs("https://qna.habr.com")))
+# print(asyncio.run(get_headers("https://qna.habr.com")))
+# print(asyncio.run(get_custom_classes("https://qna.habr.com",predict_classes1)))
+
 end_time = time.time()
 program_work_time = end_time - start_time
 print(f"Time:{program_work_time}"
       f"\n{end_time} - {start_time}")
-
-# def collect_all_divs(url_of_site):
-#     response = requests.get(url_of_site)
-#     soup = BeautifulSoup(response.content, "html.parser")
-#     divs = soup.find_all("div")
-#     return divs
-#
-#
-# def collect_all_headings(url_of_site):
-#     response = requests.get(url_of_site)
-#     soup = BeautifulSoup(response.content, "html.parser")
-#     headings = []
-#
-#     for i in range(1, 7):
-#         heading_tags = soup.find_all(f"h{i}")
-#         headings.extend(heading_tags)
-#
-#     return headings
-#
-#
-# def collect_all_href(url_of_site):
-#     response = requests.get(url_of_site)
-#     soup = BeautifulSoup(response.content, "html.parser")
-#     href_tags = soup.find_all("a", href=True)
-#
-#     href_links =  [tag["href"] for tag in href_tags]
-#
-#     return href_links
-# def get_titlesd(url_of_site: str) -> list:
-#     """Функиця, собирающая все названия """
-#     list_smth = []
-#     page_to_parse = requests.get(url_of_site, headers={'UserAgent': UserAgent().chrome})
-#     print("Устанавливаю соединение...")
-#     time.sleep(0.5)
-#     if page_to_parse.status_code == 200:
-#         print("Соединение установлено")
-#         html_code = BeautifulSoup(page_to_parse.text, "html.parser")
-#
-#         for item in html_code.findAll("title"):
-#             titles = item.text
-#             list_smth.extend(titles)
-#
-#     else:
-#         raise Exception("Ошибка: Соединение не установлено, возможно неправильно указан URL cайта.")
-#
-#     return list_smth
-
-print(asyncio.run(get_classes("https://qna.habr.com")))
-print(asyncio.run(get_title("https://qna.habr.com")))
-print(asyncio.run(get_hrefs("https://qna.habr.com")))
-print(asyncio.run(get_headers("https://qna.habr.com")))
-# print(asyncio.run(get_custom_classes("https://qna.habr.com",predict_classes1)))
