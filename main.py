9ef19ff142fafa9f759178db3ce604dbe5c399eb
Artem <artem.gavshin2459@gmail.com>
2024-04-19 17:57:40 +0300

    Добавил функцию search_without_browser

--- main.py
+++ main.py
@@ -23,7 +23,6 @@ def search_without_browser(query) -> list:
   Returns:
     Список URL-адресов результатов поиска.
   """
-    # URL поискового запроса Google
     url = f"https://www.google.com/search?q={query}"
     headers = {
         'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.3'}
