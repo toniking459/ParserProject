4c7ec920cc25321d199f476faaa4a6140c96c334
Artem <artem.gavshin2459@gmail.com>
2024-05-04 20:57:52 +0300

    .

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+import json
 from typing import List, Any
 
 from bs4 import BeautifulSoup, ResultSet
@@ -12,8 +13,29 @@ import asyncio
 import aiohttp
 from urllib.parse import urljoin
 import g4f
+from g4f.client import Client
+from g4f.Provider import Bing
+from g4f.cookies import set_cookies
+
+
+
+g4f_client = Client(text_provider=Bing)
+g4f.debug.logging = True
+
+proxies = {
+    "all": "https://77.221.157.206:3128/",
+    "https": "https://77.221.157.206:3128/"
+}
+client = Client(proxies=proxies)
+proxy = client.get_proxy()
+set_cookies(".bing.com",
+            {"_U": "14MVg1d8CRJ5aTn7A-TrBe5THL5mqGJgqhC5f3DE-kY5E27oOJjz2vJ7q8n3di_XeXw-NWLvrdOs5SVbLsTqpsZcuoKC1EejslqbjQW4_kbDCkreYhS8_2fCoLkTpQbroR4WlBL3JlEnK9mJBeSpL1fPelpszSQEbV0ttR1qJrrRMO0gO81_cdge-bvcmQtDFAVwERbM24f3TPy3Rw4sPnk2Bbcj5br-oWhuFJDiMa4R74f4SmyWQJkSOEEz1-x67LTtC_QeHiDGerKhsKA95nw"}
+)
+
 
 srch_rqst = input("Введите запрос: ")
+
+
 def search_without_browser(query) -> list:
     """
   Функция, которая выполняет поисковой запрос в Google
@@ -42,32 +64,32 @@ def search_without_browser(query) -> list:
             links1.append(corr_link)
 
     return links1
+print(search_without_browser(srch_rqst))
 
 
 def get_html_to_llm(usr_rqst: str, urls_from_search: list) -> list:
-  """
-  Отправляет контент каждой страницы из списка в ChatGPT и возвращает ответы.
+    """
+Отправляет контент каждой страницы из списка в ChatGPT и возвращает ответы.
 
-  Args:
+Args:
     usr_rqst: Пользовательский запрос.
     urls_from_search: Список URL-адресов.
 
-  Returns:
+Returns:
     Список ответов ChatGPT.
-  """
-  responses = []
-  for url in urls_from_search:
-
-      response = requests.get(url, headers={'UserAgent': UserAgent().chrome})
-      soup = BeautifulSoup(response.text, "html.parser")
-      page_text = soup.get_text(separator=" ", strip=True)
-      chat_response = g4f.ChatCompletion.create(
-          model="gpt-3.5-turbo",
-          messages=[{"role": "user", "content": f"Найди {usr_rqst} из страницы: {page_text}"}],
-      )
-      responses.append(chat_response)
-  return responses
-
+"""
+    responses = []
+    for url in urls_from_search:
+        response = requests.get(url, headers={'UserAgent': UserAgent().chrome})
+        soup = BeautifulSoup(response.text, "html.parser")
+        page_text = soup.get_text(separator=" ", strip=True)
+        chat_response = g4f_client.chat.completions.create(
+            provider=Bing, model="Copilot",
+            messages=[{"role": "user", "content": f"Найди {usr_rqst} из страницы: {url}"}],
+        )
+        responses.append(chat_response.choices[0].message.content)
+        print(page_text)
+    return responses
 
 
 # search_results = search_without_browser(srch_rqst)
@@ -76,141 +98,140 @@ def get_html_to_llm(usr_rqst: str, urls_from_search: list) -> list:
 #     match = re.match(pattern, result)
 #     if bool(match):
 #         print(result)
-
+#
 usr_rqst = input("Введите запрос для ИИ: ")
-print(get_html_to_llm(usr_rqst,search_without_browser(srch_rqst)))
-
-
 start_time = time.time()
+print(get_html_to_llm(usr_rqst, search_without_browser(srch_rqst)))
 
-
-
-async def get_classes(url_of_site: str) -> list:
-    """Асинхронная функция, собирающая названия всех классов на сайте"""
-
-    async with aiohttp.ClientSession() as session:
-        async with session.get(url_of_site, headers={'UserAgent': UserAgent().chrome}) as response:
-            print("Устанавливаю соединение...")
-            await asyncio.sleep(0.5)
-
-            if response.status == 200:
-                print("Соединение установлено")
-                html_text = await response.text(encoding='utf-8')
-                html_code = BeautifulSoup(html_text, "html.parser")
-                list_smth = []
-
-                for item in html_code.findAll(class_=True):
-                    classes = item['class']
-                    list_smth.extend(classes)
-            else:
-                raise TypeError("Ошибка: Соединение не установлено, возможно неправильно указан URL сайта.")
-
-    return list_smth
-
-
-async def get_hrefs(url_of_site: str) -> list:
-    """Асинхронная функция, собирающая все ссылки с сайта"""
-
-    async with aiohttp.ClientSession() as session:
-        async with session.get(url_of_site, headers={'UserAgent': UserAgent().chrome}) as response:
-            print("Устанавливаю соединение...")
-            await asyncio.sleep(0.5)
-
-            if response.status == 200:
-                print("Соединение установлено")
-                html_text = await response.text(encoding='utf-8')
-                html_code = BeautifulSoup(html_text, "html.parser")
-                list_smth = []
-
-                href_tags = html_code.find_all("a", href=True)
-                href_tags = [tag["href"] for tag in href_tags]
-                list_smth.extend(href_tags)
-            else:
-                raise TypeError("Ошибка: Соединение не установлено, возможно неправильно указан URL сайта.")
-    return list_smth
-
-
-async def get_headers(url_of_site: str) -> str:
-    """Асинхронная функция, собирающая все заголовки с сайта."""
-
-    async with aiohttp.ClientSession() as session:
-        async with session.get(url_of_site, headers={'UserAgent': UserAgent().chrome}) as response:
-            print("Устанавливаю соединение...")
-            await asyncio.sleep(0.5)
-
-            if response.status == 200:
-                print("Соединение установлено")
-                html_text = await response.text(encoding='utf-8')
-                html_code = BeautifulSoup(html_text, "html.parser")
-                soup = BeautifulSoup(html_text, 'lxml')
-                heading_list = []
-                for i in range(1, 7):
-                    headings = html_code.find_all(f'h{i}')
-                    heading_list.extend(headings)
-
-            else:
-                raise TypeError("Ошибка: Соединение не установлено, возможно неправильно указан URL сайта.")
-    return f"Headings: {heading_list}\n h1: {soup.h1.text}, \t h2: {soup.h2.text}, \t h3: {soup.h3.text}, \t h4: {soup.h4.text}, \t h5: {soup.h5} , \t h6: {soup.h6.text}"
-
-
-async def get_title(url_of_site: str) -> list:
-    """Асинхронная функция, собирающая все названия"""
-    list_smth = []
-
-    async with aiohttp.ClientSession() as session:
-        async with session.get(url_of_site, headers={'UserAgent': UserAgent().chrome}) as response:
-            print("Устанавливаю соединение...")
-            await asyncio.sleep(0.5)
-
-            if response.status == 200:
-                print("Соединение установлено")
-                html_text = await response.text(encoding='utf-8')
-                html_code = BeautifulSoup(html_text, "html.parser")
-
-                for item in html_code.findAll("title"):
-                    titles = item
-                    list_smth.extend(titles)
-            else:
-                raise TypeError("Ошибка: Соединение не установлено, возможно неправильно указан URL сайта.")
-    return list_smth
-
-
-predict_classes1 = (
-    'container',
-    'logo-wrapper',
-    'logo'
-
-)
-
-
-async def get_custom_classes(url_of_site: str, predict_classes: tuple) -> list:
-    async with aiohttp.ClientSession() as session:
-        async with session.get(url_of_site, headers={'UserAgent': UserAgent().chrome}) as response:
-            print("Устанавливаю соединение...")
-            await asyncio.sleep(0.5)
-
-            if response.status == 200:
-                print("Соединение установлено")
-                html_text = await response.text(encoding='utf-8')
-                html_code = BeautifulSoup(html_text, "html.parser")
-                list_smth = []
-
-                for item in html_code.findAll(class_=True):
-                    classes = item['class']
-                    list_smth.extend(classes)
-
-                result = []
-
-                for predict_class in predict_classes:
-                    for i in range(len(list_smth)):
-                        if list_smth[i] in predict_class:
-                            elements = html_code.find('div', {"class": f'{list_smth[i]}'})
-                            result.append(elements)
-
-                return result
-            else:
-                raise TypeError("Ошибка: Соединение не установлено, возможно неправильно указан URL сайта.")
-
+#
+#
+#
+# async def get_classes(url_of_site: str) -> list:
+#     """Асинхронная функция, собирающая названия всех классов на сайте"""
+#
+#     async with aiohttp.ClientSession() as session:
+#         async with session.get(url_of_site, headers={'UserAgent': UserAgent().chrome}) as response:
+#             print("Устанавливаю соединение...")
+#             await asyncio.sleep(0.5)
+#
+#             if response.status == 200:
+#                 print("Соединение установлено")
+#                 html_text = await response.text(encoding='utf-8')
+#                 html_code = BeautifulSoup(html_text, "html.parser")
+#                 list_smth = []
+#
+#                 for item in html_code.findAll(class_=True):
+#                     classes = item['class']
+#                     list_smth.extend(classes)
+#             else:
+#                 raise TypeError("Ошибка: Соединение не установлено, возможно неправильно указан URL сайта.")
+#
+#     return list_smth
+#
+#
+# async def get_hrefs(url_of_site: str) -> list:
+#     """Асинхронная функция, собирающая все ссылки с сайта"""
+#
+#     async with aiohttp.ClientSession() as session:
+#         async with session.get(url_of_site, headers={'UserAgent': UserAgent().chrome}) as response:
+#             print("Устанавливаю соединение...")
+#             await asyncio.sleep(0.5)
+#
+#             if response.status == 200:
+#                 print("Соединение установлено")
+#                 html_text = await response.text(encoding='utf-8')
+#                 html_code = BeautifulSoup(html_text, "html.parser")
+#                 list_smth = []
+#
+#                 href_tags = html_code.find_all("a", href=True)
+#                 href_tags = [tag["href"] for tag in href_tags]
+#                 list_smth.extend(href_tags)
+#             else:
+#                 raise TypeError("Ошибка: Соединение не установлено, возможно неправильно указан URL сайта.")
+#     return list_smth
+#
+#
+# async def get_headers(url_of_site: str) -> str:
+#     """Асинхронная функция, собирающая все заголовки с сайта."""
+#
+#     async with aiohttp.ClientSession() as session:
+#         async with session.get(url_of_site, headers={'UserAgent': UserAgent().chrome}) as response:
+#             print("Устанавливаю соединение...")
+#             await asyncio.sleep(0.5)
+#
+#             if response.status == 200:
+#                 print("Соединение установлено")
+#                 html_text = await response.text(encoding='utf-8')
+#                 html_code = BeautifulSoup(html_text, "html.parser")
+#                 soup = BeautifulSoup(html_text, 'lxml')
+#                 heading_list = []
+#                 for i in range(1, 7):
+#                     headings = html_code.find_all(f'h{i}')
+#                     heading_list.extend(headings)
+#
+#             else:
+#                 raise TypeError("Ошибка: Соединение не установлено, возможно неправильно указан URL сайта.")
+#     return f"Headings: {heading_list}\n h1: {soup.h1.text}, \t h2: {soup.h2.text}, \t h3: {soup.h3.text}, \t h4: {soup.h4.text}, \t h5: {soup.h5} , \t h6: {soup.h6.text}"
+#
+#
+# async def get_title(url_of_site: str) -> list:
+#     """Асинхронная функция, собирающая все названия"""
+#     list_smth = []
+#
+#     async with aiohttp.ClientSession() as session:
+#         async with session.get(url_of_site, headers={'UserAgent': UserAgent().chrome}) as response:
+#             print("Устанавливаю соединение...")
+#             await asyncio.sleep(0.5)
+#
+#             if response.status == 200:
+#                 print("Соединение установлено")
+#                 html_text = await response.text(encoding='utf-8')
+#                 html_code = BeautifulSoup(html_text, "html.parser")
+#
+#                 for item in html_code.findAll("title"):
+#                     titles = item
+#                     list_smth.extend(titles)
+#             else:
+#                 raise TypeError("Ошибка: Соединение не установлено, возможно неправильно указан URL сайта.")
+#     return list_smth
+#
+#
+# predict_classes1 = (
+#     'container',
+#     'logo-wrapper',
+#     'logo'
+#
+# )
+#
+#
+# async def get_custom_classes(url_of_site: str, predict_classes: tuple) -> list:
+#     async with aiohttp.ClientSession() as session:
+#         async with session.get(url_of_site, headers={'UserAgent': UserAgent().chrome}) as response:
+#             print("Устанавливаю соединение...")
+#             await asyncio.sleep(0.5)
+#
+#             if response.status == 200:
+#                 print("Соединение установлено")
+#                 html_text = await response.text(encoding='utf-8')
+#                 html_code = BeautifulSoup(html_text, "html.parser")
+#                 list_smth = []
+#
+#                 for item in html_code.findAll(class_=True):
+#                     classes = item['class']
+#                     list_smth.extend(classes)
+#
+#                 result = []
+#
+#                 for predict_class in predict_classes:
+#                     for i in range(len(list_smth)):
+#                         if list_smth[i] in predict_class:
+#                             elements = html_code.find('div', {"class": f'{list_smth[i]}'})
+#                             result.append(elements)
+#
+#                 return result
+#             else:
+#                 raise TypeError("Ошибка: Соединение не установлено, возможно неправильно указан URL сайта.")
+#
 
 # print(asyncio.run(get_classes("https://qna.habr.com")))
 # print(asyncio.run(get_title("https://qna.habr.com")))
